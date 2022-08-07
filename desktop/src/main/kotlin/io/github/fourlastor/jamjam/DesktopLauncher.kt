6de929ddc84c8ee621b233e784fb7b7bab105261
Daniele Conti <fourlastor@users.noreply.github.com>
2022-08-07 17:42:09 +0100

    Make the window a square

--- desktop/src/main/kotlin/io/github/fourlastor/jamjam/DesktopLauncher.kt
+++ desktop/src/main/kotlin/io/github/fourlastor/jamjam/DesktopLauncher.kt
@@ -6,9 +6,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 fun main() {
     val config = Lwjgl3ApplicationConfiguration().apply {
         val displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode()
-        val width = displayMode.width * 0.75f
-        val height = width / 16f * 9f
-        setWindowedMode(width.toInt(), height.toInt())
+        val size = minOf(displayMode.width * 0.75f, displayMode.height * 0.75f)
+        setWindowedMode(size.toInt(), size.toInt())
         setForegroundFPS(60)
     }
     Lwjgl3Application(Game(), config)
